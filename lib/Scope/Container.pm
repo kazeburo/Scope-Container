7045458db54eaa1650ce4c97fd558ae71a14e46d
Masahiro Nagano <kazeburo@gmail.com>
2010-10-26 01:23:54 +0900

    first commit

--- lib/Scope/Container.pm
+++ lib/Scope/Container.pm
@@ -1,22 +1,79 @@
 package Scope::Container;
+
 use strict;
 use warnings;
+use Guard;
+use Log::Minimal;
+use base qw/Exporter/;
+
 our $VERSION = '0.01';
+our @EXPORT = qw/start_scope_container scope_container/;
+my $CONTEXT;
+
+sub start_scope_container {
+    my $old;
+    $old = $CONTEXT if defined $CONTEXT;
+    $CONTEXT = {};
+    return guard {
+        undef $CONTEXT;
+        $CONTEXT = $old if defined $old;
+    };
+}
+
+sub scope_container {
+    my $key = shift;
+    die "undefined key" if ! defined $key;
+    debugf("scope_container is not initilized") if ! defined $CONTEXT;
+    if ( @_ ) {
+        return $CONTEXT->{$key} = shift;
+    }
+    return if ! exists $CONTEXT->{$key};
+    $CONTEXT->{$key};
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Scope::Container -
+Scope::Container - scope based container
 
 =head1 SYNOPSIS
 
   use Scope::Container;
 
+  my $contaier = start_scope_container();
+
+  sub getdb {
+      if ( my $dbh = scope_container('db') ) {
+          return $dbh;
+      } else {
+          my $dbh = DBI->connect(...);
+          scope_container('db', $dbh)
+          return $dbh;
+      }
+  }
+
+  getdb(); # do connect
+  getdb(); # from container
+
+  undef $container; #disconnect from db
+
+  getdb(); # do connect
+
 =head1 DESCRIPTION
 
-Scope::Container is
+Scope::Container is scope based container for temporary items and Database Connections.
+
+=head1 EXPORTED FUNCTION
+
+=over 4
+
+=item my $guard = start_scope_container();
+
+=item my $value = scope_container($key:Str[,$val:Str]);
+
+=back
 
 =head1 AUTHOR
 
---
+++ t/01_basic.t
@@ -0,0 +1,40 @@
+use strict;
+use Test::More;
+use Scope::Container;
+
+{
+    my $sc = start_scope_container();
+    scope_container('foo', 'foo1');
+    is( scope_container('foo'), 'foo1' );
+}
+
+ok( ! scope_container('foo') );
+
+sub bar {
+    my ($key, $val) = @_;
+    is(scope_container($key), $val);
+}
+
+{
+    my $sc = start_scope_container();
+    scope_container('foo', 'foo2');
+    bar('foo','foo2');
+    {
+        bar('foo','foo2');
+        my $sc = start_scope_container();
+        ok( ! scope_container('foo') );
+        scope_container('foo', 'foo3');
+        bar('foo','foo3');
+        {
+            my $sc = start_scope_container();
+            scope_container('foo', 'foo4');
+        }
+        is(scope_container('foo'), 'foo3');
+        bar('foo','foo3');
+    }
+    is(scope_container('foo'), 'foo2');
+}
+
+ok( ! scope_container('foo') );
+
+done_testing;
